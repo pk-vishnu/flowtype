98b02eccd8b62e1630e17ef2b33026d9a065d04d
unknown <vishnuvpk05@gmail.com>
2024-03-27 14:47:53 +0530

    fix: WebPlayer Styling

--- client/src/Player.js
+++ client/src/Player.js
@@ -5,7 +5,19 @@ export default function Player({ accessToken, trackUri }) {
 
   return (
     <div className="bg-dark">
-      <SpotifyPlayer token={accessToken} uris={trackUri ? [trackUri] : []} />
+      <SpotifyPlayer
+        token={accessToken}
+        uris={trackUri ? [trackUri] : []}
+        styles={{
+          bgColor: "transparent",
+          color: "#c4c3ce",
+          trackArtistColor: "#c4c3ce",
+          trackNameColor: "#c4c3ce",
+          sliderColor: "#62cd6a",
+        }}
+        hideAttribution="true"
+        layout="responsive"
+      />
     </div>
   );
 }
